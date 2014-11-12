c6a9aaa3981b41edffc92270e26e1b4aa665bbb5
James Moore <jmoore7@rosemary.umw.edu>
2014-11-12 15:58:57 -0500

    Finished!!!! thank goodness

--- jumble.cpp
+++ jumble.cpp
@@ -17,6 +17,7 @@ using namespace std;
 // functions protypes
 string descramble(string &, const BinaryTree &);
 string missingLetter(const string &, const string&);
+string AnswerDescrambler(string &, const BinaryTree&);
 int main(int argc, char *argv[]){
 	
 	if(argc != 2){
@@ -33,12 +34,17 @@ int main(int argc, char *argv[]){
 	// opens the  word file
 	ifstream wordList;
 	wordList.open("words.txt");
+	// create a vector!!!
 	vector<string> dict;
 	string words;
 	int count = 0;
 	// puts wordList into words
 	while(wordList){
 	wordList >> words;
+	// this make all the words uppercase
+	for(int i = 0; i < words.length(); i++){
+		words[i] = toupper(words[i]);
+	}
 	dict.push_back(words);
 	count++;
 	}
@@ -48,56 +54,36 @@ int main(int argc, char *argv[]){
 	for(int i= 0; i < count; i++){
 		dictionary.insert(dict[i], dict[i]);
 	}// end for 
-	string line1, line2, line3, line4, question, answer;
+	// these are most of what the varibles for the next few lines
+	string line1, line2, line3, line4, question, answer, finalAnswer;
 	string unscrambled1, unscrambled2, unscrambled3, unscrambled4;
+	// get the info from the file!
 	getline(file, line1);
 	getline(file, line2);
 	getline(file, line3);
 	getline(file, line4);
 	getline(file, question);
-	getline(file, answer);
-	int length1 = line1.length();
-	int length2 = line2.length();
-	int length3 = line3.length();
-	int length4 = line4.length();
-	for(int i =0; i < length1; i++){
-		line1[i] = tolower(line1[i]);
-	}// end for
- 	for(int i =0; i < length2; i++){
-		line2[i] = tolower(line2[i]);
-	}
-	for(int i =0; i < length3; i++){
-		line3[i] = tolower(line3[i]);
-	}
-	for(int i =0; i < length4; i++){
-		line4[i] = tolower(line4[i]);
-	}
+	getline(file, answer, '_');
+	// send the words the the descramble fuction
 	unscrambled1 = descramble(line1 , dictionary); 
 	unscrambled2 = descramble(line2 , dictionary); 
 	unscrambled3 = descramble(line3 , dictionary); 
 	unscrambled4 = descramble(line4 , dictionary); 
-	
+	// Need more varibles and then run the missing letter function
 	string Letter1, Letter2, Letter3, Letter4;
 	Letter1 = missingLetter(line1 , unscrambled1);
-	cout << "This is what returns " << Letter1 << endl;
 	Letter2 = missingLetter(line2 , unscrambled2);
-	cout << "This is what returns " << Letter2 << endl;
 	Letter3 = missingLetter(line3 , unscrambled3);
-	cout << "This is what returns " << Letter3 << endl;
 	Letter4 = missingLetter(line4 , unscrambled4);
-	cout << "This is what returns " << Letter4 << endl;
-	
+	// now Add the strings together then run the AnswerDescrambled function
 	string scrambledLetters = Letter1 + Letter2 + Letter3 + Letter4;
-	cout << scrambledLetters << " this is scrambledLetter" << endl;
-
-
-
+	finalAnswer = AnswerDescrambler(scrambledLetters, dictionary);
+	// now print out the final!!!
 	cout << unscrambled1 << endl << unscrambled2 << endl;
 	cout << unscrambled3 << endl << unscrambled4 << endl;
 	cout << question << endl;
-	cout << answer << endl;
+	cout << answer << " " << finalAnswer << endl;
 
-cout << "this program will do really cool stuff...." << endl;
 
 }// end main
 
@@ -105,7 +91,6 @@ cout << "this program will do really cool stuff...." << endl;
 // then it returns the descrambled word!
 string descramble(string &scrambledWord, const BinaryTree &Dict){
 	string answerWord;
-	cout << scrambledWord << " ScrambledWord" << endl;
 	//This is the dummy proof test. is the word passing through 
 	//an actual word? if so return no need to go any furter
 	answerWord = Dict.getValue(scrambledWord);
@@ -114,20 +99,15 @@ string descramble(string &scrambledWord, const BinaryTree &Dict){
 	}// end if
 	//We now know that what is being passed through is not a real word.
 	int length = scrambledWord.length();
-	cout << length << " before math" << endl; 
 	length = (length/ 2);
 	char word[length];
-	cout << length << " The Length" << endl;
 	//changing the string in to a char so we can use the next_permutation
 	for(int i = 0; i < length; i++){
 		word[i] = scrambledWord[i];
 	}// end for
-	cout << word << " before sort" << endl;
 	sort(word, word+length);
-	cout << word << " after sort" << endl;
 	//use the next_permutation and the dictionary to find the word!
 	while(next_permutation(word, word+length)){
-		cout << word << " permutation " << endl;
 		answerWord = Dict.getValue(word);
 		if(answerWord != "Null"){
 			return answerWord;
@@ -139,21 +119,50 @@ string descramble(string &scrambledWord, const BinaryTree &Dict){
 // this functions is used to find which position of the missing letters and return them.
 // then you will "add all the letters up" in a string then descramble the word!
 string missingLetter(const string & scrambledLine, const string &word){
+	//lets create a char array to hold letters to return
 	char letters[100];
 	int length = scrambledLine.length();
+	// now use half the length to create a new array
 	int halfLength = (length/2);
 	char Line[halfLength];
 	int a = halfLength+1;
+	// lets copy the orginal to scrambledLine to a char array half the size
 	for(int j = 0; j < halfLength; j++){
 		Line[j] = scrambledLine[a];
 		a++;
 	}
+	//lets find where all the "O" then compare where they are in the word then copy
+	// copy the letter at index "O" to the letters array
 	int n = 0;
 	for(int i = 0; i < halfLength; i++){
-		if(Line[i] == 'o'){
+		if(Line[i] == 'O'){
 			letters[n] = word[i];
 			n++;
 		}// end if	 
 	}// end for
+	// at the end of letters[n] put a null treminal then return
+	letters[n] = '\0';
 	return string(letters);
+}// end function
+
+
+// This shall de scramble the answer!!
+string AnswerDescrambler(string & word, const BinaryTree & Dict){
+	string answerWord;
+	// get length
+	int length = word.length();
+	// the dummy check. 
+	answerWord = Dict.getValue(word);
+	if(answerWord != "Null"){
+		return answerWord;
+	}// end if
+	// sort the word so we get all the permutation
+	sort(word.begin(), word.end());
+	//use the next_permutation and the dictionary to find the word!
+	while(next_permutation(word.begin(), word.end())){
+		answerWord = Dict.getValue(word);
+		if(answerWord != "Null"){
+			return answerWord;
+		}// end if
+	}// end while
 }// end function
